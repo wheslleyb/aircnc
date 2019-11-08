fbc22dc20003e4043bd7f0329642650a4210c7cb
Wheslley <wheslley_rimar@hotmail.com>
2019-11-08 20:48:18 -0300

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,4 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-app.listen(3333);
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
